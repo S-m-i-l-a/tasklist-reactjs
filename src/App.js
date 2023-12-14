2d8fc5bc0b54abc2220b7f611d0979fc96f48a83
S-m-i-l-a <111999212+S-m-i-l-a@users.noreply.github.com>
2023-12-14 21:56:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleInputChange}
+          placeholder="Add a task..."
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoApp;
